327de749abb9ee79fcf62aa5e18f23fadea4c96b
StefanoGitHub <stefano.borghi@gmail.com>
2016-12-11 20:18:42 -0800

    added d3 examples

--- samplesapp/static/js/showsamples.js
+++ samplesapp/static/js/showsamples.js
@@ -46,27 +46,33 @@ nodes.forEach(function(node, i) {
     }
 });
 
+// define drawing area
 var myChart = d3.select('#graph')
     .append('svg')
     .attr('width', w)
     .attr('height', h);
 
+// create graph layout
 var force = d3.layout.force()
     .nodes(nodes)
     .links([])
     .gravity(.1)
+    .linkDistance(150)
     .charge(-1000)
     .size([w, h]);
 
+// create graph's links
 var link = myChart.selectAll('line')
     .data(links).enter().append('line')
     .attr('stroke', palette.gray);
 
+// create nodes
 var node = myChart.selectAll('circle')
     .data(nodes).enter()
     .append('g')
     .call(force.drag);
 
+// draw nodes
 node.append('circle')
     .attr('cx', function (d) {
         return d.x;
@@ -80,8 +86,11 @@ node.append('circle')
             return palette.pink;
         }
         return palette.blue;
-    });
+    })
+    .attr('stroke', palette.lightgray)
+    .attr('stroke-width', 2);
 
+// draw labels
 node.append('text')
     .text(function (d) {
         return d.name;
@@ -115,11 +124,13 @@ node.append('text')
         return '1em'
     });
 
+// define tick marks
 force.on('tick', function (e) {
+    // update nodes position
     node.attr('transform', function (d, i) {
         return 'translate(' + d.x + ', ' + d.y + ')';
     });
-
+    // update links position
     link
         .attr('x1', function (d) {
             return d.source.x
@@ -132,14 +143,46 @@ force.on('tick', function (e) {
         })
         .attr('y2', function (d) {
             return d.target.y
-        })
+        });
+
+    // var q = d3.geom.quadtree(nodes);
+    //
+    // nodes.forEach(function(n) {
+    //     q.visit(collide(n));
+    // });
 });
 
-node.on('click', function (e) {
-    force.gravity(0)
-        .charge(0);
-});
+// node.on('click', function (e) {
+//     force.gravity(0)
+//         .charge(0);
+// });
 
+// function collide(node) {
+//     var r = node.radius + 16,
+//         nx1 = node.x - r,
+//         nx2 = node.x + r,
+//         ny1 = node.y - r,
+//         ny2 = node.y + r;
+//     return function (quad, x1, y1, x2, y2) {
+//         if (quad.point && (quad.point !== node)) {
+//             var x = node.x - quad.point.x,
+//                 y = node.y - quad.point.y,
+//                 l = Math.sqrt(x * x + y * y),
+//                 r = node.radius + quad.point.radius;
+//             if (l < r) {
+//                 l = (l - r) / l * .5;
+//                 node.x -= x *= l;
+//                 node.y -= y *= l;
+//                 quad.point.x += x;
+//                 quad.point.y += y;
+//             }
+//         }
+//         return x1 > nx2
+//             || x2 < nx1
+//             || y1 > ny2
+//             || y2 < ny1;
+//     };
+// }
 
 force.start();
 
